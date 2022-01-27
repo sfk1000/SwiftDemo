dfdf348d49367b81959ede4d17e8b000bfcbfc04
Jong <47660067+sfk1000@users.noreply.github.com>
2022-01-27 17:53:35 +0900

    앱 실행시 업데이트 선택적 알림창 구현

--- WebViewDemo/ContentView.swift
+++ WebViewDemo/ContentView.swift
@@ -3,7 +3,7 @@
 //  WebViewDemo
 //
 //  Created by Clip-1152 on 2022/01/20.
-//
+//  Start 2022
 
 import SwiftUI
 import WebKit
@@ -26,8 +26,75 @@ struct ContentView: View {
     @State private var isShowPhotoLibrary = false
     @State private var image = UIImage()
     
+    //Build Number (Currebt Project Version)
+    func currentBuildNumber() -> String {
+      if let info: [String: Any] = Bundle.main.infoDictionary,
+          let buildNumber: String
+            = info["CFBundleVersion"] as? String {
+            return buildNumber
+      }
+      return "nil"
+    }
+    
+    //App Version (Marketing Version)
+    func currentAppVersion() -> String {
+      if let info: [String: Any] = Bundle.main.infoDictionary,
+          let currentVersion: String
+            = info["CFBundleShortVersionString"] as? String {
+            return currentVersion
+      }
+      return "nil"
+    }
+    
+    @State private var presentAlert = true
+    
+    
+    //apple ID
     var body: some View {
         
+        let targetBuildVersion = currentBuildNumber()
+        let targetAppVersion = currentAppVersion()
+        
+        
+        let oldVersion = "0.11.2"
+        let lastVersion = "0.12.1"
+        
+        let compareResult = oldVersion.compare(lastVersion, options: .numeric)
+        
+        
+        
+        VStack {
+            Text(presentAlert ? "True 2" : "False 2").alert(isPresented: $presentAlert, content: {
+                    
+                    Alert(
+                        title: Text("선택적 업데이트 안내"),
+                        message: Text("앱 변경 사항이 있습니다."),
+                        primaryButton: .destructive(Text("업데이트")) {
+                            print("앱 업데이트로 이동...")
+                        },
+                        secondaryButton: .cancel()
+                    )
+                }
+            )
+            
+            Text(targetBuildVersion)
+            Text(targetAppVersion)
+            
+            switch compareResult {
+                //lastVersion 이 클 경우
+                case .orderedAscending:
+                    Text("T1")
+                //lastVersion 이 작을 경우
+                case .orderedDescending:
+                    Text("T2")
+                case .orderedSame:
+                    Text("T3")
+            }
+            
+        }
+        
+        
+        /*
         let urlString = "https://www.naver.com"
         if let url = URL(string: urlString) {
             let urlReq = URLRequest(url: url)
@@ -36,7 +103,7 @@ struct ContentView: View {
         else{
                 Text("Try Again")
         }
-         
+        */
     }
 }
 
